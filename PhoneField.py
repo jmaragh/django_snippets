a6ef42932745ba36357f3ef793c195e54136da87
jmaragh <88741414+jmaragh@users.noreply.github.com>
2021-10-29 12:04:22 -0700

    Update PhoneField.py

--- PhoneField.py
+++ PhoneField.py
@@ -1,29 +1,10 @@
 from django.core.validators import RegexValidator
+from django.forms import CharField
+from django.forms.widgets import TextInput
 
-class PhoneField(MultiValueField):
+class PhoneField(CharField):
+    widget = TextInput
+    default_validators = [RegexValidator(r"^\d{3,4}-\d{3}-\d{4}$", "Invalid Phone number")]
+
     def __init__(self, **kwargs):
-        # Define one message for all fields.
-        error_messages = {
-            'incomplete': 'Enter a country calling code and a phone number.',
-        }
-        # Or define a different message for each field.
-        fields = (
-            CharField(
-                error_messages={'incomplete': 'Enter a country calling code.'},
-                validators=[
-                    RegexValidator(r'^[0-9]+$', 'Enter a valid country calling code.'),
-                ],
-            ),
-            CharField(
-                error_messages={'incomplete': 'Enter a phone number.'},
-                validators=[RegexValidator(r'^[0-9]+$', 'Enter a valid phone number.')],
-            ),
-            CharField(
-                validators=[RegexValidator(r'^[0-9]+$', 'Enter a valid extension.')],
-                required=False,
-            ),
-        )
-        super().__init__(
-            error_messages=error_messages, fields=fields,
-            require_all_fields=False, **kwargs
-        )
+        return super().__init__(strip=True, **kwargs)
